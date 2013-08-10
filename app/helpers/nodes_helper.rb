22d90240e8e92ba387163efe17502b90532c603c
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2013-08-10 22:52:30 +0000

    Prettier UI

---
+++ app/helpers/nodes_helper.rb
@@ -0,0 +1,24 @@
+module NodesHelper
+  def node_report_time(node)
+    if node.last_report
+      time_ago_in_words(node.last_report) 
+    else
+      "unreported"
+    end
+  end
+
+  def node_status_badge(node)
+    if node.status
+      case node.status
+        when "changed"
+          content_tag(:span, node.status, :class => 'badge badge-success')
+        when "unchanged"
+          content_tag(:span, node.status, :class => 'badge')
+        when "failed"
+          content_tag(:span, node.status, :class => 'badge badge-important')
+      end
+    else
+      content_tag(:span, "unreported", :class => 'badge')
+    end
+  end
+end
