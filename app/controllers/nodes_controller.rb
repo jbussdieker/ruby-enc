3985488498a565c2ef6f2836a6a45446b5dd11fc
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2013-08-10 22:59:40 +0000

    Default to last_report desc

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -44,10 +44,10 @@ class NodesController < ApplicationController
   private
 
   def sort_column
-    Node.column_names.include?(params[:sort]) ? params[:sort] : "name"
+    Node.column_names.include?(params[:sort]) ? params[:sort] : "last_report"
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 end
