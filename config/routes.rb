21f22b655ab99de8ae8e2cc69ee5e1bbcf1bdf05
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2013-08-10 20:12:22 +0000

    Fix dash in name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Enc::Application.routes.draw do
-  resources :nodes, :id => /[A-Za-z0-9\.]+?/, :format => /json|csv|xml|yaml/
+  resources :nodes, :id => /[A-Za-z0-9\-\.]+?/, :format => /json|csv|xml|yaml/
   resources :reports do
     member do
       get 'parse'
