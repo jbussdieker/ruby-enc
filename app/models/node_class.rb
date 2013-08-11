05c911d4e4caf60ecc3c0d27c95f2fe1bfcc170d
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-11 12:13:17 -0400

    If a class gets deleted delete it's memberships

--- app/models/node_class.rb
+++ app/models/node_class.rb
@@ -1,6 +1,6 @@
 class NodeClass < ActiveRecord::Base
   attr_accessible :name
 
-  has_many :node_class_memberships
+  has_many :node_class_memberships, :dependent => :destroy
   has_many :nodes, :through => :node_class_memberships
 end
