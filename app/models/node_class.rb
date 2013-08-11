b9096cea58f7738b8e80f193490310021328389b
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-11 15:51:28 -0400

    Default to order by name

--- app/models/node_class.rb
+++ app/models/node_class.rb
@@ -1,6 +1,8 @@
 class NodeClass < ActiveRecord::Base
   attr_accessible :name
 
+  default_scope order(:name)
+
   has_many :node_class_memberships, :dependent => :destroy
   has_many :nodes, :through => :node_class_memberships
 end
