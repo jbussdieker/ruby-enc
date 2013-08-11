d99ebddf292801b59e98feddbbae96b9d7465571
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-11 12:13:02 -0400

    If a group gets deleted then delete it's memberships and parameters

--- app/models/node_group.rb
+++ app/models/node_group.rb
@@ -1,7 +1,7 @@
 class NodeGroup < ActiveRecord::Base
   attr_accessible :name
 
-  has_many :node_group_memberships
+  has_many :node_group_memberships, :dependent => :destroy
   has_many :nodes, :through => :node_group_memberships
-  has_many :parameters, :as => :parameterable
+  has_many :parameters, :as => :parameterable, :dependent => :destroy
 end
