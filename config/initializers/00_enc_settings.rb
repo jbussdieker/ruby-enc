f290c439fcf1603a6ccecdbba162598c2dc46d68
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-13 19:20:12 -0400

    For testing

--- config/initializers/00_enc_settings.rb
+++ config/initializers/00_enc_settings.rb
@@ -1,1 +1,6 @@
-ENC_CONFIG = YAML.load_file("#{Rails.root}/config/settings.yml")[Rails.env].with_indifferent_access
+if File.exists? "config/settings.yml"
+  ENC_CONFIG = YAML.load_file("#{Rails.root}/config/settings.yml")[Rails.env].with_indifferent_access
+else
+  puts "WARNING: No config/settings.yml file found"
+  ENC_CONFIG = {}
+end
