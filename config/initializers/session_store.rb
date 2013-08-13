5516a8d08c7821e5e1ee47d31c952c0fc34420c9
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-12 20:42:54 -0400

    Fix hash syntax

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Enc::Application.config.session_store :cookie_store, key: '_enc_session'
+Enc::Application.config.session_store :cookie_store, :key => '_enc_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
