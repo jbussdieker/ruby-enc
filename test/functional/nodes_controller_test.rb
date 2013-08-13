41878f6a14e5b9a919ae110c639a7b23c014ad7f
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-13 16:53:07 -0400

    Fix some test breakages

--- test/functional/nodes_controller_test.rb
+++ test/functional/nodes_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class NodesControllerTest < ActionController::TestCase
   setup do
-    @node = nodes(:one)
+    @node = nodes(:node_one)
   end
 
   test "should get index" do
