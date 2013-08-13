ba93715e7460d55cb175ad7bec0835828f6aec9e
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-13 11:57:04 -0400

    More parameter cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,5 +21,5 @@ function remove_fields(link) {
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).parent().before(content.replace(regexp, new_id));
+  $(link).before(content.replace(regexp, new_id));
 }
