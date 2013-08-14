7ea1cd063d98507766b0614e4a78bf344f77ec52
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-14 19:26:39 -0400

    Placeholder for node deactivate

--- lib/puppetdb/node.rb
+++ lib/puppetdb/node.rb
@@ -10,6 +10,11 @@ module PuppetDB
       @resource ||= Client.new.request("/v2/nodes/#{name}/resources")
     end
 
+    def deactiveate
+      Client.new.request("/v1/commands")
+      #curl -XPOST /v1/commands -H "Accept: application/json" -d 'payload={"command":"deactivate node","version":1,"payload":"\"#{name}\""}' -v
+    end
+
     def resource_types
       resources.collect {|r| r["type"]}.uniq
     end
