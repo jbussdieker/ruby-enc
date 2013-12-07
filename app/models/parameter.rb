09a4066e40ce4e1eecd59ee463da75b8a32aa596
Joshua Bussdieker <jbussdieker@gmail.com>
2013-12-07 16:21:31 -0500

    Make sure the key is unique within the resource

--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -1,6 +1,8 @@
 class Parameter < ActiveRecord::Base
   attr_accessible :key, :parameterable_id, :parameterable_type, :value
 
+  validates_uniqueness_of :key, :scope => [:parameterable_id, :parameterable_type]
+
   validates_presence_of :key
   validates_presence_of :value
 end
