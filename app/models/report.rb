f36701c7087fbcbf4c37a159ee368ba5162903c5
Joshua Bussdieker <jbussdieker@gmail.com>
2013-12-08 16:38:16 -0500

    Faster delete of reports

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,8 +4,7 @@ class Report < ActiveRecord::Base
   attr_accessible :node_id, :status, :environment, :time
   belongs_to :node
 
-  # TODO: What should we do here
-  has_many :report_logs, :dependent => :delete_all #:dependent => :destroy
-  has_many :metrics, :dependent => :delete_all #:dependent => :destroy
-  has_many :resource_statuses, :dependent => :delete_all #:dependent => :destroy
+  has_many :report_logs, :dependent => :delete_all
+  has_many :metrics, :dependent => :delete_all
+  has_many :resource_statuses, :dependent => :delete_all
 end
