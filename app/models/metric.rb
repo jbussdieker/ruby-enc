882069075fdfd4e913a0bfb7dda35f5338904310
Joshua Bussdieker <jbussdieker@gmail.com>
2013-12-08 16:35:56 -0500

    Database actually seems to require this

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -2,4 +2,6 @@ class Metric < ActiveRecord::Base
   attr_accessible :category, :name, :report_id, :value
 
   belongs_to :report
+
+  validates_presence_of :report_id
 end
--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -4,4 +4,8 @@ describe Metric do
   it "has a valid factory" do
     FactoryGirl.create(:metric).should be_valid
   end
+
+  it "is invalid without a report" do
+    FactoryGirl.build(:metric, report_id: nil).should_not be_valid
+  end
 end
