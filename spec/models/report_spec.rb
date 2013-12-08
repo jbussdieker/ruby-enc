4f6f08375888452f193fb5daabd02c1ee2765cd0
Joshua Bussdieker <jbussdieker@gmail.com>
2013-12-08 16:38:53 -0500

    Test dependent deletion

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -4,4 +4,28 @@ describe Report do
   it "has a valid factory" do
     FactoryGirl.create(:report).should be_valid
   end
+
+  context "dependents" do
+    before(:each) do
+      @report = FactoryGirl.create(:report_with_dependents)
+    end
+
+    it "deletes metrics" do
+      expect {
+        @report.destroy
+      }.to change(Metric, :count).by(-5)
+    end
+
+    it "deletes report logs" do
+      expect {
+        @report.destroy
+      }.to change(ReportLog, :count).by(-5)
+    end
+
+    it "deletes resource statuses" do
+      expect {
+        @report.destroy
+      }.to change(ResourceStatus, :count).by(-5)
+    end
+  end
 end
