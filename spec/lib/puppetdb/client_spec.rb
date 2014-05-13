d505dc87c4285559d1b54b6176e4b5eee5a62c79
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-05-12 18:12:20 -0700

    Start specs for puppetdb stuff

---
+++ spec/lib/puppetdb/client_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe PuppetDB::Client do
+  describe "request" do
+  end
+end
---
+++ spec/lib/puppetdb/node_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe PuppetDB::Node do
+  describe "facts" do
+  end
+
+  describe "resources" do
+  end
+
+  describe "deactivate" do
+  end
+
+  describe "resource_types" do
+  end
+end
