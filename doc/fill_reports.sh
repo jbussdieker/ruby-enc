239308749cdf638fa17a1b8604d5a25b4277a26e
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-05-15 00:39:46 -0700

    This should default to normal port

--- doc/fill_reports.sh
+++ doc/fill_reports.sh
@@ -2,7 +2,7 @@
 for version in spec/fixtures/reports/*; do
   for type in changed failed unchanged pending; do
     echo $version/$type.yaml
-    curl -XPOST localhost:9292/reports/upload --data-binary @$version/$type.yaml
+    curl -XPOST localhost:3000/reports/upload --data-binary @$version/$type.yaml
     echo
   done
 done
