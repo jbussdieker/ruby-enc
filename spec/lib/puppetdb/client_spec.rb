02634042f5f9011166855a6a9c68b327ed0e697a
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-05-18 11:35:48 -0700

    Add spec since the changes got deployed

--- spec/lib/puppetdb/client_spec.rb
+++ spec/lib/puppetdb/client_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe PuppetDB::Client do
-  describe "request" do
+  let(:options) { {} }
+  let(:client) { PuppetDB::Client.new(options) }
+  subject { client }
+
+  describe 'client' do
+    subject { client.client }
+
+    it { should be_kind_of Net::HTTP }
   end
 end
