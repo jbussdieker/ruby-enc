600def1487f56e3259c08bf41223e6d5a414fa4b
Joshua B. Bussdieker <jbussdieker@gmail.com>
2016-01-31 04:09:00 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140509184854) do
+ActiveRecord::Schema.define(:version => 20160101000000) do
 
   create_table "metrics", :force => true do |t|
     t.integer  "report_id",                                 :null => false
